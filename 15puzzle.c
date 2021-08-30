f754fc55a270337f49748511bcecc12311d32ef1
TPKato <44572590+TPKato@users.noreply.github.com>
2021-08-30 15:44:37 +0200

    version 1.2 (1997/06/04 11:46:23)

--- 15puzzle.c
+++ 15puzzle.c
@@ -2,8 +2,13 @@
 #include <sys/types.h>
 #include <sys/time.h>
 
-#define TILE_X 5
-#define TILE_Y 3
+#ifndef SCOREFILE
+#define SCOREFILE "/var/games/15puzzle.score"
+#endif
+
+#define DEFAULT_TILE_X 4
+#define DEFAULT_TILE_Y 3
+
 #define TSHUFFLE 500
 
 #define UP	(1 << 0)
@@ -11,15 +16,30 @@
 #define LEFT	(1 << 2)
 #define RIGHT	(1 << 3)
 
-#define SPACE 15
+#define SPACE 15		/* 4 x 4 - 1 */
 
-#define puttile(x) mvwin(tile[pos[x]], TILE_Y * ((x) / 4), TILE_X * (int)((x) % 4));
+#define puttile(x) mvwin(tile[pos[x]], tile_y * ((x) / 4), tile_x * (int)((x) % 4));
 
 WINDOW *tile[16];
 int pos[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, SPACE};
 unsigned int turn = 0;
 double drand48();
-char scorefile[] = "15puzzle.score";
+int tile_x = 0, tile_y = 0, height = 0, width = 0;
+
+const char revision[] = "$Revision: 1.2 $";
+
+void usage()
+{
+  puts("15puzzle [OPTION]...\n");
+  puts("  -r     reverse mode");
+  printf("  -x n   tile width  (>= 2, default is %d)\n", DEFAULT_TILE_X);
+  printf("  -y n   tile height (>= 1, default is %d)\n", DEFAULT_TILE_Y);
+  puts("  -X n   game width  (not supported yet)");
+  puts("  -Y n   game height (not supported yet)");
+  puts("  -s     display score and exit");
+  puts("  -h     display this help and exit");
+  puts("  -v     output version information and exit");
+}
 
 void wininit()
 {
@@ -31,17 +51,17 @@ void wininit()
   leaveok(stdscr, TRUE);
 
   for (i = 0; i < 15; i++) {
-    tile[i] = newwin(TILE_Y, TILE_X, TILE_Y * (i / 4), TILE_X * (int)(i % 4));
+    tile[i] = newwin(tile_y, tile_x, tile_y * (i / 4), tile_x * (int)(i % 4));
     box(tile[i], '|', '-');
     mvwaddch(tile[i], 0, 0, '+');
-    mvwaddch(tile[i], TILE_Y - 1, 0, '+');
-    mvwaddch(tile[i], 0, TILE_X - 1, '+');
-    mvwaddch(tile[i], TILE_Y - 1, TILE_X - 1, '+');
-    mvwprintw(tile[i], 1, 1, "%2d", i + 1);
+    mvwaddch(tile[i], tile_y - 1, 0, '+');
+    mvwaddch(tile[i], 0, tile_x - 1, '+');
+    mvwaddch(tile[i], tile_y - 1, tile_x - 1, '+');
+    mvwprintw(tile[i], (int)(tile_y / 2), (int)(tile_x / 2) - 1, "%2d", i + 1);
     leaveok(tile[i], TRUE);
     wrefresh(tile[i]);
   }
-  tile[i] = newwin(TILE_Y, TILE_X, TILE_Y * (i / 4), TILE_X * (int)(i % 4));
+  tile[i] = newwin(tile_y, tile_x, tile_y * (i / 4), tile_x * (int)(i % 4));
   leaveok(tile[i], TRUE);
   wrefresh(tile[i]);
 }
@@ -111,68 +131,122 @@ int finished()
 void shuffle()
 {
   int i, dir;
-  
+
   for (i = 0; i < TSHUFFLE; i++) {
     dir = (int)(drand48() * 4);
     movetile(1<<dir);
   }
 }
 
+void showscore()
+{
+  puts(" Name                    Times    Turns");
+  puts("----------------------------------------");
+  system("sort -n +2 -t: " SCOREFILE " | head -10 | nawk '{split($0, a, \":\") ; printf(\" %-20s %8d %8d\\n\", a[1], a[2], a[3])}'");
+  puts("----------------------------------------");
+}
+
 void score(long time)
 {
-  char name[256];
+  char name[256], *i;
   FILE *fp;
+  WINDOW *input;
 
-  printf("Finished! (%ld sec, %d turns)\n", time, turn);
+  input = newwin(0, 0, tile_y * 4, 0);
+  /* --- 4 -> height */
+  scrollok(input, TRUE);
+  echo();
 
-  puts("Your name (less than 16chars)\nvvvvvvvvvvvvvvv");
-  gets(name);
+  wprintw(input, "Finished! (%ld sec, %d turns)\n", time, turn);
 
-  if((fp = fopen(scorefile, "a")) == NULL) {
-    perror(scorefile);
-    exit(1);
+  waddstr(input, "Your name (less than 20chars)\nvvvvvvvvvvvvvvvvvvvv\n");
+  wrefresh(input);
+  wgetstr(input, name);
+
+  name[20] = '\0';
+
+  /* translate ':' -> ' ' */
+  i = name;
+  while (*i) {
+    if (*i == ':') *i = ' ';
+    i++;
   }
 
-  fprintf(fp, "%-16s %8ld %8d\n", name, time, turn);
+  if((fp = fopen(SCOREFILE, "a")) == NULL) {
+    perror(SCOREFILE);
+    exit(1);
+  }
+  fprintf(fp, "%s:%ld:%d\n", name, time, turn);
+  fclose(fp);
+
+  endwin();
+  showscore();
 }
 
 int main(int argc, char **argv)
 {
   time_t start, end;
-  int rev = 0;
+  int rev = 0, c;
+  extern char *optarg;
+  extern int optind;
 
-  if (argc > 1) {
-    if (strncmp(argv[1], "-rev", 4)) {
-      rev = 0;
-    } else {
+  /* arguments */
+  while ((c = getopt(argc, argv, "rx:y:X:Y:hvs")) != -1) {
+    switch (c) {
+    case 'r':
       rev = (UP | DOWN | LEFT | RIGHT);
+      break;
+    case 'x':
+      tile_x = atoi(optarg);
+      break;
+    case 'y':
+      tile_y = atoi(optarg);
+      break;
+    case 'X':
+      height = atoi(optarg);
+      break;
+    case 'Y':
+      width = atoi(optarg);
+      break;
+    case 'h':
+      usage();
+      exit(1);
+      break;
+    case 'v':
+      fprintf(stderr, "15puzzle: %s\n", revision);
+      exit(1);
+      break;
+    case 's':
+      showscore();
+      exit(1);
+      break;
+    case '?':
+      puts("Try `15puzzle -h' for more information.");
+      exit(1);
+      break;
     }
   }
+
+  if (! tile_x) tile_x = DEFAULT_TILE_X;
+  if (! tile_y) tile_y = DEFAULT_TILE_Y;
 
   srand48((long)time(NULL));
   wininit();
   shuffle();
 
-  printf(">%d<", rev);
-
   (void)time(&start);
 
   do {
     switch (getch()) {
-    case 'k':
-      if (rev) movetile(DOWN); else movetile(UP); break;
-    case 'j':
-      if (rev) movetile(UP); else movetile(DOWN); break;
-    case 'h':
-      if (rev) movetile(RIGHT); else movetile(LEFT); break;
-    case 'l':
-      if (rev) movetile(LEFT); else movetile(RIGHT); break;
+    case 'k': movetile(UP    ^ ((UP | DOWN) & rev)); break;
+    case 'j': movetile(DOWN  ^ ((UP | DOWN) & rev)); break;
+    case 'h': movetile(LEFT  ^ ((LEFT | RIGHT) & rev)); break;
+    case 'l': movetile(RIGHT ^ ((LEFT | RIGHT) & rev)); break;
     }
   } while (! finished());
 
   (void)time(&end);
 
-  endwin();
   score((long)(end - start));
 
   return 0;
--- Makefile
+++ Makefile
@@ -5,8 +5,12 @@ CFLAGS = -g -O2
 LDLIBS = -lcurses -ltermcap
 
 
-15puzzle : 15puzzle.o
+15puzzle : 15puzzle.o 15puzzle.score
 	$(CC) $(LDFLAGS) $< $(LDLIBS) -o $@
+
+15puzzle.score :
+	touch $@
+	chmod 666 $@
 
 clean :
 	$(RM) *.o *.obj *~ *.bak
